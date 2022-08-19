f2da94a77fde2170cae17f051bbc8cc7039cd070
VLx2000 <victor.antunes.2000@gmail.com>
2022-08-19 11:35:06 -0300

    spinner e msg em register

--- src/pages/Login/resgister.tsx
+++ src/pages/Login/resgister.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Alert, Button, Container, Form, Spinner } from "react-bootstrap";
 import { AuthContext } from "contexts/auth";
 import './styles.css';
 import axiosInstance from "utils/axios";
@@ -12,10 +12,12 @@ function Register() {
     const [name, setName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [carregado, setCarregado] = useState<Boolean>(true);
+    const [message, setMessage] = useState<string>('');
 
     const submitHandler = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-
+        setCarregado(false);
         axiosInstance
             .post('/users/register', 
                 JSON.stringify({ name, email, password }),
@@ -25,43 +27,52 @@ function Register() {
                 context?.login!(email, password);
             })
             .catch((error) => {
-                console.log('epa')
+                setMessage(error.response.data.message);  
             });
+        setCarregado(true);
     };
 
     return (
         <Container className="login-container">
-            <Form onSubmit={submitHandler}>
-                <h3 className="titulo-pag">Registrar-se</h3>
-                <Form.Group controlId="newNome" className="mb-3">
-                    <Form.Label column sm="6">Nome</Form.Label>
-                    <Form.Control
-                        required
-                        onChange={(e) => setName(e.target.value as string)}
-                    />
-                </Form.Group>
-                <Form.Group controlId="newEmail" className="mb-3">
-                    <Form.Label column sm="6">Email</Form.Label>
-                    <Form.Control
-                        required
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value as string)}
-                        placeholder="name@example.com"
-                    />
-                </Form.Group>
-                <Form.Group controlId="newSenha" className="mb-3">
-                    <Form.Label column sm="6">Senha</Form.Label>
-                    <Form.Control
-                        required
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value as string)}
-                    />
-                </Form.Group>
-                <Form.Group controlId="criar" className="mb-3 div-botao-entrar">
-                    <Button type="submit">Criar conta</Button>
-                </Form.Group>
-                <div>Já tem uma conta? <Link to={'/login'}>Logar-se</Link></div>
-            </Form>
+            {carregado ?
+                <Form onSubmit={submitHandler}>
+                    <h3 className="titulo-pag">Registrar-se</h3>
+                    {message && <Alert variant="warning">{message}</Alert>}
+                    <Form.Group controlId="newNome" className="mb-3">
+                        <Form.Label column sm="6">Nome</Form.Label>
+                        <Form.Control
+                            required
+                            onChange={(e) => setName(e.target.value as string)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="newEmail" className="mb-3">
+                        <Form.Label column sm="6">Email</Form.Label>
+                        <Form.Control
+                            required
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value as string)}
+                            placeholder="name@example.com"
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="newSenha" className="mb-3">
+                        <Form.Label column sm="6">Senha</Form.Label>
+                        <Form.Control
+                            required
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value as string)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="criar" className="mb-3 div-botao-entrar">
+                        <Button type="submit">Criar conta</Button>
+                    </Form.Group>
+                    <div>Já tem uma conta? <Link to={'/login'}>Logar-se</Link></div>
+                </Form>
+                : <div className="d-flex justify-content-center">
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div> 
+            }
         </Container>
     );
 }
